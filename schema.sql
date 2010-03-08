cb51e702243a7f2469a0342690de0b357c2d54a6
Rob Ellis <kazoomer@gmail.com>
2010-03-08 11:35:03 -0800

    Added index to schema

--- schema.sql
+++ schema.sql
@@ -6,6 +6,8 @@ CREATE TABLE committer (
   created_at date DEFAULT NULL
 );
 
+CREATE UNIQUE INDEX committer_idx ON committer (name);
+
 CREATE SEQUENCE map_seq;
 
 CREATE TABLE host_committer_map (
@@ -14,10 +16,14 @@ CREATE TABLE host_committer_map (
   committer_id integer DEFAULT NULL
 );
 
-CREATE SEQUENCE hosts_seq;
+CREATE UNIQUE INDEX hosts_committer_idx ON host_committer_map (host_id,committer_id);
 
+
+CREATE SEQUENCE hosts_seq;
 CREATE TABLE hosts (
   id INTEGER NOT NULL PRIMARY KEY DEFAULT nextval('hosts_seq'),
   hosts varchar(50) DEFAULT NULL,
   created_at date DEFAULT NULL
 );
+
+CREATE UNIQUE INDEX hosts_idx ON hosts (hosts);
