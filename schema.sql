e41ad76bdce196209dd4552d714929298b92a819
Scott McWhirter <scott.mcwhirter@joyent.com>
2010-04-01 10:03:21 -0700

    Tidy up schema DDL and add owner_id

--- schema.sql
+++ schema.sql
@@ -1,29 +1,20 @@
-CREATE SEQUENCE committer_seq;
 
 CREATE TABLE committer (
-  id INTEGER NOT NULL PRIMARY KEY DEFAULT nextval('committer_seq'),
-  name varchar(30) DEFAULT NULL,
-  created_at date DEFAULT NULL
+    id SERIAL PRIMARY KEY,
+    name varchar(30) NOT NULL UNIQUE,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
-CREATE UNIQUE INDEX committer_idx ON committer (name);
-
-CREATE SEQUENCE map_seq;
+CREATE TABLE hosts (
+    id SERIAL PRIMARY KEY,
+    hosts varchar(250) NOT NULL UNIQUE,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    owner_id INTEGER REFERENCES committer (id) ON DELETE CASCADE
+);
 
 CREATE TABLE host_committer_map (
-  id INTEGER NOT NULL PRIMARY KEY DEFAULT nextval('map_seq'),
-  host_id integer DEFAULT NULL,
-  committer_id integer DEFAULT NULL
+    host_id INTEGER REFERENCES hosts (id) ON DELETE CASCADE,
+    committer_id INTEGER REFERENCES committer (id) ON DELETE CASCADE,
+    PRIMARY KEY ( host_id, committer_id )
 );
 
-CREATE UNIQUE INDEX hosts_committer_idx ON host_committer_map (host_id,committer_id);
-
-
-CREATE SEQUENCE hosts_seq;
-CREATE TABLE hosts (
-  id INTEGER NOT NULL PRIMARY KEY DEFAULT nextval('hosts_seq'),
-  hosts varchar(50) DEFAULT NULL,
-  created_at date DEFAULT NULL
-);
-
-CREATE UNIQUE INDEX hosts_idx ON hosts (hosts);
